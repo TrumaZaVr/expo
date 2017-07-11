52cf6cd4553a9a0b083da35bbeed914ee25675a7
TrumaZaVr <trumvovaz@gmail.com>
2017-07-12 00:31:24 +0300

    09.07456

---
+++ about.php
@@ -0,0 +1,17 @@
+<?php
+$Firstname = 'Владимир';
+$Age = '24';
+$Email = 'trumvovaz@gmail.com';
+$Town = 'Королев';
+$About = 'Разнорабочий';
+?>
+<div>
+    <h2>Страницйа пользователя</h2>
+    <p>Имя:<blockquote><strong><?=$Firstname?></blockquote></p>
+    <p>Возраст:<blockquote><strong><?=$Age?></blockquote></p>
+    <p>Адрес электронной почты:<blockquote><strong><?=$Email?></blockquote></p>
+    <p>Город<blockquote><strong><?=$Town?></blockquote></p>
+    <p>О себе:<blockquote><strong><?=$About?></blockquote></p>
+</div>
+
+
